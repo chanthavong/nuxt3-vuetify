91c48e31f73af172ae9a1d33004191a65e75c17b
Mark AI <chanthavong_12@hotmail.com>
2022-10-16 12:02:05 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue", "nuxt"],
+  rules: {
+    semi: "off",
+    "no-console": 1,
+    "vue/require-default-prop": "off",
+    "vue/attributes-order": "off",
+    "vue/one-component-per-file": "off",
+    "vue/html-closing-bracket-newline": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/attribute-hyphenation": "off",
+    "space-before-function-paren": "off",
+    "vue/script-setup-uses-vars": 1,
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "vue/custom-event-name-casing": "off",
+    "vue/no-unused-components": "warn",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "vue/no-v-model-argument": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-btn variant="flat" color="primary" @click="sayHi" class="mb-4"
+        >Click me</v-btn
+      >
+      <v-alert type="success">I'm a success alert.</v-alert>
+
+      <br />
+
+      <v-alert type="info">I'm an info alert.</v-alert>
+
+      <br />
+
+      <v-alert type="warning">I'm a warning alert.</v-alert>
+
+      <br />
+
+      <v-alert type="error">I'm an error alert.</v-alert>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+const sayHi = () => alert("Hello world");
+</script>
